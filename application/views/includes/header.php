b3c7263fc9db7d5b5e40c8d253769e759d1f9e25
Alibaltistani <muhammadalid15@gmail.com>
2024-08-24 14:29:55 +0500

    header link change

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -221,7 +221,7 @@
                   $txt =  "transportation";
                 }
                 else if ($cLabel == DESERT) {
-                  $txt =  DESERT;
+                  $txt =  "transportation";
                 }
                 if (check_permission($service->categoryName, 'list') == 1) { ?>
 
